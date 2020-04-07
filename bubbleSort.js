40fa7d342a67c570e2766fcf867f01ebe77d1648
AkshatJen <akshatjain5555@gmail.com>
2020-04-07 23:26:35 +0100

    removed -2 for testing

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,5 +1,5 @@
 // array of unsorted numbers
-const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1, -2];
+const arr = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, -1];
 
 //ES6 fat syntax bubble sort function
 const bubbleSort = (arr) => {
