4cd6592408ac1608aee6c5d8234c8064afb952d6
AkshatJen <akshatjain5555@gmail.com>
2020-04-14 17:39:33 +0100

    prettied the entire file

--- filterMapReduce.js
+++ filterMapReduce.js
@@ -6,7 +6,9 @@ console.log("\n==========Original Array============== \n");
 console.log(arr);
 console.log("\n");
 
-let filteredArray = arr.filter((x) => typeof(parseInt(x))=="number" && x%2 == 1);
+let filteredArray = arr.filter(
+  (x) => typeof parseInt(x) == "number" && x % 2 == 1
+);
 
 // this will give an empty array because the difference between the
 // "==" which is an equality operator check && "===" is identity operator check
@@ -14,29 +16,37 @@ let filteredArray = arr.filter((x) => typeof(parseInt(x))=="number" && x%2 == 1)
 // in case of === it should give a [] empty array since the the values inside the array dont match the type
 
 //filter function is basically a condition check which can be applied to every individual item in the array
-//or list and reduces it to the element which have met the conditions truely 
+//or list and reduces it to the element which have met the conditions truely
 // we can also filter the string aswell if desired
 
 //console.log(arr.filter(x=> (x%2 == false)|| (typeof(x) == "string")));
 
-console.log("===============filtered array of odd numbers====================== \n");
+console.log(
+  "===============filtered array of odd numbers====================== \n"
+);
 console.log(filteredArray + "\n\n");
 
 //mapping works on all the elements of the array and does a certain task, here to square
 
-let mappedArray = filteredArray.map(x=> x*x);
-console.log("============mapped array of square of odd numbers================= \n");
+let mappedArray = filteredArray.map((x) => x * x);
+console.log(
+  "============mapped array of square of odd numbers================= \n"
+);
 console.log(mappedArray + "\n\n");
 
 // reduces does as the name suggests, to reduce the list into one
 
-let reducedArray = mappedArray.reduce((x,y)=>x+y);
-console.log("=======reduced array of addition of squares of odd numbers======== \n");
+let reducedArray = mappedArray.reduce((x, y) => x + y);
+console.log(
+  "=======reduced array of addition of squares of odd numbers======== \n"
+);
 console.log(reducedArray + "\n\n");
 
-
 // using chaining method can also help us achieve similar results
-// code above is equivalent to the code below 
-let filteredMapReducedArray = arr.filter(x=> typeof(parseInt(x)) === "number" && x%2===1).map(x=>x*x).reduce((x,y)=>x+y);
+// code above is equivalent to the code below
+let filteredMapReducedArray = arr
+  .filter((x) => typeof parseInt(x) === "number" && x % 2 === 1)
+  .map((x) => x * x)
+  .reduce((x, y) => x + y);
 
 console.log(filteredMapReducedArray);
