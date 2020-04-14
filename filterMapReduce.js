8cf671a5ebc94be8add39afded00b70921b3b957
AkshatJen <akshatjain5555@gmail.com>
2020-04-14 14:28:19 +0100

    JS filter added Array.prototype.filter is a functional way to filter out values based on the callback function

---
+++ filterMapReduce.js
@@ -0,0 +1,8 @@
+// this is a demo on how to use the filter map and reduce 
+// we are also going to e learning on how to chain them together
+
+const arr = [0,1,2,3,4,5,6,7,8];
+
+let filteredArray = arr.filter(x=> x%2 == 1);
+
+console.log(filteredArray);
