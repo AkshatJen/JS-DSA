6a5dd7a623f6adc14eca1f4d7ac3c163bcc80bc0
AkshatJen <akshatjain5555@gmail.com>
2020-04-20 23:47:51 +0100

    mergeSortCompleted

--- mergeSort.js
+++ mergeSort.js
@@ -7,5 +7,32 @@ const mergeSort = (arr) => {
   const left = arr.slice(0, middle);
   const right = arr.slice(middle);
 
-  return arr;
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
 };
+
+
+const merge = (left,right) => {
+    const resultArray =[];  
+    while(left.length && right.length){
+        if(left[0] <= right[0]){
+            resultArray.push(left.shift());
+        }
+        else{
+            resultArray.push(right.shift());
+        }
+    }
+    while(left.length){
+        resultArray.push(left.shift());
+    }
+    while(right.length){
+        resultArray.push(right.shift());
+    }
+    return resultArray;
+}
+
+const arr = [4,5,6,1,2,3];
+
+console.log(mergeSort(arr));
