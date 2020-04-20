3a53c63806d7fe569c3ae959b04bacd7962d7af2
AkshatJen <akshatjain5555@gmail.com>
2020-04-20 23:21:25 +0100

    mergeSortFormatting

--- mergeSort.js
+++ mergeSort.js
@@ -1,12 +1,11 @@
-const mergeSort = arr => {
-    if(arr.length < 2){
-        return arr;
-    }
-    const length = arr.length;
-    const middle = Math.floor(length/2);
-
-    const left = arr.slice(0,middle);
-    const right = arr.slice(middle);
-
+const mergeSort = (arr) => {
+  if (arr.length < 2) {
     return arr;
-}
+  }
+  const length = arr.length;
+  const middle = Math.floor(length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return arr;
+};
