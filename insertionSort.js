68bf5567e2ac29bc23316d2789a611669d32199b
AkshatJen <akshatjain5555@gmail.com>
2020-04-20 01:32:52 +0100

    insertion sort completed

--- insertionSort.js
+++ insertionSort.js
@@ -19,7 +19,7 @@ const arr1 = ["smile", "love", "sad", "angry", "hearbreak", "coded", "debug","an
 
 const arr = [3,4,5,6,1,2];
 
-console.log(insertionSort(arr1));
+console.log(insertionSort(arr));
 // slice is used to obtain a new array, think it as a slicing something
 // slice (from, to element[i-1]) 
 // [0,1,2,3].slice(1,3) -> would give [1,2]
