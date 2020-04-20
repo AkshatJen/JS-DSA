ad77e6d5215eddd2e4bd4592bce23c57b6f68e09
AkshatJen <akshatjain5555@gmail.com>
2020-04-20 21:21:21 +0100

    insertion sort completed and edited

--- insertionSort.js
+++ insertionSort.js
@@ -1,25 +1,21 @@
-const insertionSort = arr => {
+const insertionSort = arr =>{
 
-    for(let i = 1 ; i < arr.length ; i++){
-
-        for(let j = 0; j < i ; j++){
-            if(arr[i] < arr[j]){
+    for (let i = 1 ; i < arr.length ; i++ ){
+        for(let j = 0 ; j < i ; j++){
+            if(arr[i]<arr[j]){
                 let spliced = arr.splice(i,1);
                 arr.splice(j,0,spliced[0]);
+
             }
         }
-
     }
-
     return arr;
-} 
-
-
+}
 const arr1 = ["smile", "love", "sad", "angry", "hearbreak", "coded", "debug","anxious"];
 
-const arr = [3,4,5,6,1,2];
+const arr = [3,7,6,1,4,8,2,5];
 
-console.log(insertionSort(arr));
+//console.log(insertionSort(arr));
 // slice is used to obtain a new array, think it as a slicing something
 // slice (from, to element[i-1]) 
 // [0,1,2,3].slice(1,3) -> would give [1,2]
@@ -31,4 +27,4 @@ console.log(insertionSort(arr));
 // splice (index, counts , new value)
 
 //arr.splice(3,0,"happy");
-//console.log(arr);
+console.log(insertionSort(arr));
