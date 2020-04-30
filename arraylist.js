bae8a704f928c92730927d45cb198b2b34dac79a
AkshatJen <akshatjain5555@gmail.com>
2020-05-01 00:21:30 +0100

    prettied_ArrayList

--- arraylist.js
+++ arraylist.js
@@ -1,46 +1,45 @@
 class ArrayList {
+  contructor() {
+    this.length = 0;
+    this.data = {};
+  }
 
-    contructor(){
+  push(value) {
+    this.data[this.length] = value;
+    this.length++;
+  }
 
-        this.length = 0;
-        this.data = {};
+  pop() {
+    const ans = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return ans;
+
+    /*
+or could be done using the delete too
+return this.delete(this.length-1);
+     */
+  }
+  get(index) {
+    return this.data[index];
+  }
+
+  delete(index) {
+    const ans = this.data[index];
+    this._collapseTo(index);
+    return ans;
+  }
+
+  _collapseTo(index) {
+    for (let i = index; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
     }
-
-            push(value){
-                this.data[this.length] = value;
-                this.length++;
-            }
-
-            pop(){
-                const ans = this.data[this.length-1];
-                delete this.data[this.length-1];
-                this.length--;
-                return ans;
-
-                 /*
-                 or could be done using the delete too
-
-                 return this.delete(this.length-1);
-                 */
-            }
-            get(index){
-                return this.data[index];
-            }
-
-            delete(index){
-
-                const ans = this.data[index];
-                this._collapseTo(index);
-                return ans;
-            }
-
-            _collapseTo(index){
-
-                for(let i = index; i< this.length; i++){
-                    this.data[i] = this.data[i+1];
-
-                }
-                delete this.data[this.length-1];
-                this.length --;
-            }
+    delete this.data[this.length - 1];
+    this.length--;
+  }
 }
+
+const arr = new ArrayList();
+
+arr.push(5);
+console.log(arr);
