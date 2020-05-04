5a0347c918336e2bf7dd65f73dd9284eec559fdc
AkshatJen <akshatjain5555@gmail.com>
2020-05-04 16:52:03 +0100

    closure_added_🔥

--- Memoization/memoizeFactorial.js
+++ Memoization/memoizeFactorial.js
@@ -49,6 +49,37 @@ const factorialIterativeCache = index => {
 }
 
 
-console.log(factorialIterativeCache(20));
+//console.log(factorialIterativeCache(20));
 //console.log(cache);
-console.log(factorialIterativeCache(20));
+//console.log(factorialIterativeCache(20));
+
+
+// 4. best approach with similiar performance as to above while keeping the cache hidden from all the other global 
+
+const factorialIterativeCacheClosure = () =>{
+    const cache = {};
+
+    return(index)=>{
+
+    if(cache[index]) {
+        //console.log("returned from the cache");
+        return cache[index];
+    }
+
+    const results = [1];
+
+    for(let i = 0 ; i<index ; i++){
+        //console.log("=====loop number=====" + i);
+        results.push(results[i] * (i+1));
+        cache[i+1] = results[i] * (i+1);
+    }
+    //console.log(results);
+    return results[index];
+    }
+}
+
+const fib100 = factorialIterativeCacheClosure();
+const fibNumber = fib100(20);
+console.log(fibNumber);
+// console.log(fib100(19));
+// console.log(fib100(18));
