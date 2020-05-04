66c7382af1906a1bf12b080bcc0ed1955c23f1f7
AkshatJen <akshatjain5555@gmail.com>
2020-05-04 23:28:30 +0100

    commented_version_of_mergeSort

--- Sorting Algorithm/mergeSort.js
+++ Sorting Algorithm/mergeSort.js
@@ -1,25 +1,40 @@
 const mergeSort = (arr) => {
   if (arr.length < 2) {
+    console.log("now its size 1");
     return arr;
   }
   const length = arr.length;
   const middle = Math.floor(length / 2);
   const left = arr.slice(0, middle);
   const right = arr.slice(middle);
+  console.log("divided into two");
 
-  const sortedLeft = mergeSort(left);
-  const sortedRight = mergeSort(right);
+  // const sortedLeft = mergeSort(left);
+  // const sortedRight = mergeSort(right);
 
-  return merge(sortedLeft, sortedRight);
+  // return merge(sortedLeft, sortedRight);
+  console.log(left);
+  console.log(right);
+
+  return merge(mergeSort(left), mergeSort(right));
 };
 
 const merge = (left, right) => {
+  console.log("entering the merge");
   const resultArray = [];
   while (left.length && right.length) {
     if (left[0] <= right[0]) {
+      console.log("entering left less then right")
+      console.log(resultArray)
       resultArray.push(left.shift());
+      console.log("entering left less then right DONE")
+      console.log(resultArray)
     } else {
+      console.log("entering left bigger then right")
+      console.log(resultArray)
       resultArray.push(right.shift());
+      console.log("entering left bigger then right DONE")
+      console.log(resultArray);
     }
   }
   /*  while (left.length) {
@@ -31,7 +46,8 @@ const merge = (left, right) => {
   //return resultArray;
 
   //return resultArray.concat(left,right);
-
+  console.log("Concatanating happening")
+  console.log(resultArray + left + right);
   return [...resultArray, ...left, ...right];
 };
 
